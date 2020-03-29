1bd4ff9a5329e43eb949f9b1078b5e19000bf1ab
senatorQn <mail@kuhnp.de>
2020-03-29 17:43:05 +0200

    make factors list and table dynamic

---
+++ custom.js
@@ -0,0 +1,145 @@
+// $(function () {
+//   $('.sortable').sortable();
+//   $('.handles').sortable({
+//     handle: 'span'
+//   });
+//   $('.connected').sortable({
+//     connectWith: '.connected'
+//   });
+//   $('.exclude').sortable({
+//     items: ':not(.disabled)'
+//   });
+// });
+
+var factors = ["Personal", "Fertigung", "Verwaltung", "Auftragseingang", "Materiallieferung", "Liquidität"];
+var scenarios = ["Faktor", "Szenario 1", "Szenario 2", "Szenario 3", "Szenario 4"];
+
+function makeUL(array) {
+  // Create the list element:
+  var list = document.createElement('ul');
+  list.setAttribute('id', 'list');
+
+  for (var i = 0; i < array.length; i++) {
+    // Create the list item:
+    var item = document.createElement('li');
+
+    // var span = document.createElement("span");
+    // span.appendChild(document.createTextNode(':: '));
+    // item.appendChild(span)
+
+    item.setAttribute('id', i);
+    item.setAttribute('draggable', 'true')
+    // Set its contents:
+    item.appendChild(document.createTextNode(array[i]));
+
+    // Add it to the list:
+    list.appendChild(item);
+  }
+
+  // Finally, return the constructed list:
+  return list;
+}
+
+function updateList() {
+  var list = document.getElementById('thelist')
+  list.innerHTML = "";
+  list.appendChild(makeUL(factors));
+}
+
+function makeTable(array) {
+  tbl = document.createElement('table');
+  tbl.style.width = '100px';
+
+  let thead = tbl.createTHead();
+  let row = thead.insertRow();
+  for (let key of scenarios) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+
+  for (let element of factors) {
+    let row = tbl.insertRow();
+    var i = 0;
+    for (; i < scenarios.length; i++) {
+      let cell = row.insertCell();
+      if (i == 0) {
+        let text = document.createTextNode(element);
+        cell.appendChild(text);
+      }
+    }
+  }
+  return tbl;
+}
+
+function updateTable() {
+  var list = document.getElementById('thetable')
+  list.innerHTML = "";
+  list.appendChild(makeTable(factors));
+}
+
+updateList();
+updateTable();
+
+function addItem() {
+  var candidate = document.getElementById("candidate");
+  if (!!candidate.value) {
+    factors.push(candidate.value);
+    updateList();
+    updateTable();
+  }
+}
+
+function removeItem() {
+  if (!!factors) {
+    for (var i = 0; i < selected.length; i++) {
+      factors.splice(selected[i], 1);
+      updateList();
+      updateTable();
+    }
+  }
+}
+
+var selected = []
+
+function updateSelected() {
+  items = document.getElementsByClassName('selected');
+  var i;
+  selected = []
+  for (i = 0; i < items.length; i++) {
+    selected.push(items[i].id);
+  }
+}
+
+var ulDiv = document.getElementById('thelist')
+ulDiv.onclick = function (event) {
+  if (event.target.tagName != "LI") return;
+
+  if (event.ctrlKey || event.metaKey) {
+    toggleSelect(event.target);
+    updateSelected();
+  } else {
+    singleSelect(event.target);
+    updateSelected();
+  }
+
+}
+
+// prevent unneeded selection of list elements on clicks
+ulDiv.onmousedown = function () {
+  return false;
+};
+
+function toggleSelect(li) {
+  li.classList.toggle('selected');
+}
+
+function singleSelect(li) {
+  let selected = ulDiv.querySelectorAll('.selected');
+  for (let elem of selected) {
+    elem.classList.remove('selected');
+  }
+  li.classList.add('selected');
+}
+
